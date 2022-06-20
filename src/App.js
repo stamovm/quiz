41d1349d88726f3e0d90c41553f6fd0fff375df9
stamovm <m.stamov@yahoo.com>
2022-06-19 18:44:10 -0700

    Created some components. Added Axios and react router.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { Routes, Route } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import { LinkContainer } from 'react-router-bootstrap'
+// import Button from 'react-bootstrap/Button'
+// import Badge from 'react-bootstrap/Badge'
+import Nav from 'react-bootstrap/Nav'
+import Home from './components/Home'
+import Quiz from './components/Quiz'
+import About from './components/About'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">header</header>
-      <h1>hello world!</h1>
+    <div className="App d-flex flex-column">
+      <header className="App-header">
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand href="#home">Q</Navbar.Brand>
+            </LinkContainer>
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/quiz:id">
+                <Nav.Link>Quiz</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Home = () => {
+  const [qData, setQData] = useState([])
+  async function getQuiz() {
+    try {
+      const response = await axios.get('/api/quizzes')
+      setQData(response.data.quizzes)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getQuiz()
+  }, [])
+
+  return (
+    <div>
+      <Container className="p-3">
+        {qData.map((q) => (
+          <Card key={q.id} style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title> {q.name}</Card.Title>
+              <Card.Text>Description text</Card.Text>
+              <LinkContainer to={`/quiz${q.id}`}>
+                <Button variant="primary">Start quiz</Button>
+              </LinkContainer>
+            </Card.Body>
+          </Card>
+        ))}
+      </Container>
+    </div>
+  )
+}
+
+export default Home
