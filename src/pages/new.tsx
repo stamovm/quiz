f681b01d85cc76168121cca0c134891c4d31c827
stamovm <m.stamov@yahoo.com>
2022-12-15 19:35:08 -0800

    Created a form for adding new questions.

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,12 +1,43 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 
+const Question: React.FC = () => {
+  const handleSubmit = () => {
+    // Handle
+  };
+
+  return (
+    <div className="m-2 rounded-md border border-teal-500 p-2">
+      <div className="">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Question:
+            <input type="text" className="m-2 border border-teal-600" />
+          </label>
+          <br />
+          <label>
+            Answers:
+            <input type="text" className="m-2 border border-teal-600" />
+            <input type="text" className="m-2 border border-teal-600" />
+            <input type="text" className="m-2 border border-teal-600" />
+            <input type="text" className="m-2 border border-teal-600" />
+          </label>
+          <br />
+        </form>
+      </div>
+    </div>
+  );
+};
+
 const New: NextPage = () => {
   const { data: session } = useSession();
   if (!session) return <h2>Please sign in</h2>;
   return (
     <div className="rounded-md border border-teal-500 p-2">
-      <p className="text-xl">Create a new quiz </p>
+      <p className="ml-2 text-xl">Create a new quiz </p>
+      <Question />
+      <Question />
+      <Question />
     </div>
   );
 };
